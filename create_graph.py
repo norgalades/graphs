a9821c813ebeb30888439142f2603e3a4843a29a
norgalades <30833739+norgalades@users.noreply.github.com>
2017-10-31 09:00:50 +0100

    Read and store samples from specific directory

--- create_graph.py
+++ create_graph.py
@@ -5,31 +5,33 @@ Created on 23 Oct 2017
 '''
 
 import networkx as nx
-from format_input import input_formatter
+import os
+from format_input import parse_input_dir
 
+#Inputs and variables
+inputdir1 = "METASPLOIT"
 #Format the input file so that it is possible to read the edges and write it to file output
-input_formatter('parse.txt', 'edgelist.txt')
+#parse_input_dir(inputdir1)
+inputdir1+="_PARSED"
 
-#Create a directed graph 
-DG=nx.DiGraph()
+DGs = list()
 
-#Read the edge list and populate the graph
-DG=nx.read_edgelist("edgelist.txt")
-
+#Create the database with malware's graphs for a certain family 
+sample_number=0
+for filename in os.listdir(inputdir1):
+    #Create a directed graph 
+    DGs.append(nx.read_edgelist(inputdir1+"\\"+filename, create_using=nx.DiGraph()))
+    #Read the edge list and populate the graph corresponding to sample_number
+    sample_number+=1
+    
 #Print some information on the just read graph
-print("Number of nodes: ")
-print(DG.number_of_nodes())
+print("Number of nodes in each graph\n")
+i=1
+for DG in DGs:
+    print("Graph" + str(i))
+    print(DG.number_of_nodes())
+    i+=1
+    edges = [e for e in DG.edges()]
+    print(edges)
 
-print("Edges: ")
-edges = [e for e in DG.edges.data()]
-print(edges)
-
-#Trash   
-print("Edges from/to node Scheme: ")
-l=DG.neighbors("Scheme")
-
-for node in l:
-    print(node)
-
-print(DG.has_edge("Msg", "Actor"))
 
