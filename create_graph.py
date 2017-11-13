a9b728b1b012cdb90e689ee94120ae3cc0ceaa30
norgalades <30833739+norgalades@users.noreply.github.com>
2017-11-13 13:25:52 +0100

    Added function check_node_component(G, file)
    Function that labels graph's nodes

--- create_graph.py
+++ create_graph.py
@@ -11,11 +11,44 @@ from format_input import parse_input_dir
 #Inputs and variables
 inputdir1 = "METASPLOIT"
 #Format the input file so that it is possible to read the edges and write it to file output
-#parse_input_dir(inputdir1)
+parse_input_dir(inputdir1)
 inputdir1+="_PARSED"
 
 DGs = list()
 
+#Functions
+def check_node_component(G, file):
+    #Open the corresponding AndroidManidest 
+    with open(file, 'r') as fin:
+        rows = fin.read().splitlines(True)
+            
+    #Take all the nodes contained on the graph G
+    nodes = [n for n in G.nodes()]
+    for n in nodes: 
+        #For each node, search the component type and add it as attribute
+        for row in rows:
+            if n in row: 
+                if "activity" in row:
+                    d = {n : 'activity'}
+                    nx.set_node_attributes(G, d, "compType")
+                    break #step to next node
+                elif "service" in row:
+                    d = {n : 'service'}
+                    n.set_node_attributes(G, d, "compType")
+                    break #step to next node
+                elif "provider" in row:
+                    d = {n : 'provider'}
+                    n.set_node_attributes(G, d, "compType")
+                    break #step to next node
+                elif "receiver" in row:
+                    d = {n : 'receiver'}
+                    n.set_node_attributes(G, d, "compType")
+                    break #step to next node
+        else:
+            d = {n : 'innerClass'}
+            nx.set_node_attributes(G, d, "compType")
+            
+#Main program 
 #Create the database with malware's graphs for a certain family 
 sample_number=0
 for filename in os.listdir(inputdir1):
@@ -24,14 +57,18 @@ for filename in os.listdir(inputdir1):
     #Read the edge list and populate the graph corresponding to sample_number
     sample_number+=1
     
+check_node_component(DGs[0], "AndroidManifest_da5b.xml")    
+    
 #Print some information on the just read graph
-print("Number of nodes in each graph\n")
+print("Number of nodes in each graph")
 i=1
 for DG in DGs:
     print("Graph" + str(i))
     print(DG.number_of_nodes())
     i+=1
-    edges = [e for e in DG.edges()]
-    print(edges)
+    nodes = [n for n in DG.nodes()]
+    print(nx.get_node_attributes(DG, "compType"))
+    #edges = [e for e in DG.edges()]
+    #print(edges)
 
 
